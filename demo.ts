0a2cefe1edcbea288975d151006577e47cec877d
ZhiHong Chua <chuazhihong@hotmail.com>
2023-11-11 19:06:25 +0800

    Update demo.ts

--- demo.ts
+++ demo.ts
@@ -3,7 +3,5 @@ const catalyst = 'redbull'
 const madness = 'urine'
 const moreMadness = 'coke'
 const purity = 'water'
-const shit = 'candleWax'
-const chai = 'indianTea'
 
 console.log(variable + catalyst + madness + moreMadness + purity + shit + chai) // anyone's imagination
