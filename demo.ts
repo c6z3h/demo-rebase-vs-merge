0dd379204efea8a449fab6a7d8e1bb6edd2ee077
ZhiHong Chua <chuazhihong@hotmail.com>
2023-11-11 19:26:45 +0800

    Update demo.ts

--- demo.ts
+++ demo.ts
@@ -1,6 +1,5 @@
-const variable = 'jaegermeister'
 const catalyst = 'redbull'
 const madness = 'tea'
 const moreMadness = 'coke'
 
-console.log(variable + catalyst + madness + moreMadness) // anyone's imagination
+console.log(catalyst + madness + moreMadness) // anyone's imagination
